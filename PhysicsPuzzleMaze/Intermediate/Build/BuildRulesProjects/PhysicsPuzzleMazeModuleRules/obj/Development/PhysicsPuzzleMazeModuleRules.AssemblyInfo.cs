a94dfc6d0b6a9af3ea4fbdf5b93b9e89fbb02900
Scott Lee <scottlee5455@outlook.com>
2025-11-23 15:50:40 -0600

    working on camera angles

--- PhysicsPuzzleMaze/Intermediate/Build/BuildRulesProjects/PhysicsPuzzleMazeModuleRules/obj/Development/PhysicsPuzzleMazeModuleRules.AssemblyInfo.cs
+++ PhysicsPuzzleMaze/Intermediate/Build/BuildRulesProjects/PhysicsPuzzleMazeModuleRules/obj/Development/PhysicsPuzzleMazeModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhysicsPuzzleMazeModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e098ce64f0d1b59cf47263987fc828ad1fcd8103")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09822211a26c207ef5d10ae784cac8cb83c955b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhysicsPuzzleMazeModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhysicsPuzzleMazeModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
